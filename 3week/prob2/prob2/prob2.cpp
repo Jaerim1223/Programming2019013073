5f5f4ac4bf180b10a7f743d3942832082f7b430c
Jaerim1223 <kjlim3310@naver.com>
2022-09-28 17:24:48 +0900

    prob2

---
+++ 3week/prob2/prob2/prob2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int age, chestsize;
+	char size;
+	printf("Input your age:");
+	scanf_s("%d", &age);
+
+	printf("Input your chestsize:");
+	scanf_s("%d", &chestsize);
+
+	if (age < 20)
+	{
+		if (chestsize < 85) size = 'S';
+		else if (chestsize < 95) size = 'M';
+		else size = 'L';
+	}
+	else
+	{
+		if (chestsize < 90) size = 'S';
+		else if (chestsize < 100) size = 'M';
+		else size = 'L';
+	}
+	printf("your size is %c \n", size);
+	return 0;
+}
