d1f6feb19878689c53bd2c13ddfff4c072f3b517
Jaerim1223 <113013066+Jaerim1223@users.noreply.github.com>
2022-12-07 17:03:37 +0900

    Update 14week.ino

--- 14week/14week.ino
+++ 14week/14week.ino
@@ -9,7 +9,7 @@ int previousVoltage = 0;//이전 전압의 상태를 확인
 void setup() {
   servoMot.attach(8); //서보모터 8번에 연결
  
- // for 문을 활용하여 i = 0 이라는 초기화식을 선언하고, sizeof를 통해 앞서 언급한 LED_pins의 배열을 불러와 OUTPUT(출력)으로 설정
+ // for 문을 활용하여 i = 0 이라는 초기화식을 선언하고, sizeof를 통해 앞서 언급한 LED_pins의 배열을 불러와 OUTPUT으로 설정
   for(int i = 0; i < sizeof(LED_pins) / sizeof(int); i++){
   pinMode(LED_pins[i],OUTPUT);
  
@@ -17,16 +17,16 @@ void setup() {
 }
 void loop() {
  
-
+  //서보모터 0~180도 돌아갈 수 있게 설정
     int analogValue = analogRead(sensorPin) ;
-  servoMot.write(map(analogValue,0,1023,0,180));
+    servoMot.write(map(analogValue,0,1023,0,180));
     delay (15);
      
   //아날로그 0번핀에 연결된 가변 저항의 값을 읽고, 다시 현재 전압으로 map()함수를 이용하여 변환
     int analogIn = analogRead(A0);  
     currentVoltage = map(analogIn , 0, 1023, 0, 5);
   
-  //만약에 현재 전압 currentVoltage 가 != 즉 , 이전 전압 previousVoltage 와 같지 않을때,이전 전압을 갱신  
+  //만약에 현재 전압 currentVoltage가 이전 전압 previousVoltage 와 같지 않을때,이전 전압을 갱신  
    if(currentVoltage != previousVoltage){
     previousVoltage = currentVoltage;
     
